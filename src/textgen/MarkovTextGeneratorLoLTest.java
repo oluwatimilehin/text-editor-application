fa52b42309a81daba60069fbaacceb64ea551657
Timi <oluwatimilehinadeniran@gmail.com>
2017-08-12 15:18:22 +0100

    Create test for markov generator

---
+++ src/textgen/MarkovTextGeneratorLoLTest.java
@@ -0,0 +1,22 @@
+package textgen;
+
+import org.junit.After;
+import org.junit.Before;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Oluwatimilehin on 12/08/2017.
+ */
+public class MarkovTextGeneratorLoLTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+}
